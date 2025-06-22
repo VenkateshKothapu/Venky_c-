3184e8853ece1fd00b431202c5dd7f9ac6643e6f
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-06-22 14:45:11 +0530

    datatypes

---
+++ Venky_c#/DataTypesDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class DataTypesDemo
+{
+    static void Main(string[] args)
+    {
+        int age = 25;
+        float weight = 90.9f;
+        double height = 5.11;
+        char grade = 'A';
+        bool islearning = true;
+        string name = "venkatesh";
+
+
+        Console.WriteLine("Name:"+name);
+        Console.WriteLine("Age:" + age);
+        Console.WriteLine( "Height" + height);
+        Console.WriteLine( "Weight:"+weight);
+        Console.WriteLine("Learning c#" + islearning);
+        Console.WriteLine("Grade:" + grade);
+
+    }
+}
