b488af39e86d67c870147ec76b04b8881579474a
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-06-25 14:24:34 +0530

    finding minimum and maximum in datatypes

---
+++ Venky_c#/DatatypesRange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ class DatatypesRange
+    {
+    static void Main(string[] args) 
+    {
+        Console.WriteLine($"byte :{byte.MinValue} to {byte.MaxValue}");
+        Console.WriteLine($"short :{short.MinValue} to {short.MaxValue}");
+        Console.WriteLine($"int :{int.MinValue} to {int.MaxValue}");
+        Console.WriteLine($"long :{long.MinValue} to {long.MaxValue}");
+        Console.WriteLine($"double :{double.MinValue} to {double.MaxValue}");
+        Console.WriteLine($"float : {float.MinValue} to {float.MaxValue}");
+        Console.WriteLine($"decimal :{decimal.MinValue} to {decimal.MinValue}");
+    }
+    }
+
