8a3a2abf74c22ccec57a78807b8a7d545b829e71
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-06-26 23:27:48 +0530

    var and dynamic

---
+++ Venky_c#/VarandDynamic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class VarandDynamic
+{
+    static void Main(String[] args)
+    {
+        //var message = "Hey how are you buddy";
+        //Console.WriteLine(message.ToUpper());
+
+        ////message=100; error this will not work because string cannot be changed to int after declaring using var
+
+
+
+        ////now we will se dynamic
+
+
+        dynamic Message = "hey how are you ";
+        Console.WriteLine("message :" + Message);
+
+        Message = 256;
+        Console.WriteLine(Message + 1);
+        Message = true;
+        Console.WriteLine("message :" + Message);
+
+    }
+}
