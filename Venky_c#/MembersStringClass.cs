9ac18edc31f10e0c4e0104f42b61d2459467d49f
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-06-28 02:32:17 +0530

    memebers of string

---
+++ Venky_c#/MembersStringClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class MembersStringClass
+    {
+    static void Main(String[] args)
+    {
+        string name = "venkatesh";
+        Console.WriteLine("name : "+name.Length);
+        Console.WriteLine("name Uppercase : " + name.ToUpper());
+        Console.WriteLine("name Lowercase : " + name.ToLower());
+        Console.WriteLine("Trim : " + name.Trim());
+        Console.WriteLine("Contains : " + name.Contains("venkatesh"));
+        Console.WriteLine("Contains : " + name.Contains("venky"));
+        Console.WriteLine(" Name : " + name.StartsWith("ve"));
+        Console.WriteLine("Name : " + name.EndsWith("sh"));
+
+
+    }
+ }
+
