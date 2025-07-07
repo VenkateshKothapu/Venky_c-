de070ac8cb3a0a90a49762d45bc53570eda6b28b
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-07-07 21:24:12 +0530

    Value_Type and Reference_Type

---
+++ Venky_c#/ValuetypeandReftype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+class ValuetypeandReftype
+{
+    static void Main(string[] args)
+    {
+        //value type
+        int a = 90;
+        int b = a;
+        b = 25;
+
+        Console.WriteLine(b);
+        Console.WriteLine(a);
+
+        //a and b are seperate copies . changing b doesnt affect a.
+
+        //refernce types
+        int[] arra1 = { 1, 2, 5, 6 };
+        int[] arra2 = arra1;
+        arra2[0] = 90;
+        Console.WriteLine(arra2[0]);
+        Console.WriteLine(arra1[0]);
+
+    }
+}
+
