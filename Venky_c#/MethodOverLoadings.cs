49b40a613c2ad8c54a084ada8769042d3ae143b4
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-07-28 16:02:11 +0530

    method overloading

---
+++ Venky_c#/MethodOverLoadings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MethodOverLoadings
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    static int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    static double Add(double a, double b)
+    {
+        return a + b;
+
+    }
+    static void Main()
+    {
+        Console.WriteLine("Adding two numbers " + Add(5, 10));
+        Console.WriteLine("Adding three numbers " + Add(5, 10, 15));
+        Console.WriteLine("Adding two double " + Add(5.5 ,10.5));
+    }
+}
+
